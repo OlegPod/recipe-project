a98dadd5ba7328ac0f5df2465f459cebf3b79575
OlegPod <kepacuh@gmail.com>
2019-04-07 23:20:12 +0300

    Adding Notes and Recipe

--- src/main/java/com/olehpodolin/recipeproject/controllers/IndexController.java
+++ src/main/java/com/olehpodolin/recipeproject/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index"})
     public String getIndexPage() {
+        System.out.println("Loaded123456789");
         return "index";
     }
 }
