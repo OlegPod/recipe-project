4742f67da0d9023ca9ba353bf55468d229e359ce
OlegPod <kepacuh@gmail.com>
2019-04-10 00:22:03 +0300

    Introducing JPA Enumerations

--- src/main/java/com/olehpodolin/recipeproject/domain/Recipe.java
+++ src/main/java/com/olehpodolin/recipeproject/domain/Recipe.java
@@ -25,6 +25,8 @@ public class Recipe {
 
     @Lob
     private Byte[] image;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -112,4 +114,8 @@ public class Recipe {
     public Set<Ingredient> getIngredients() { return ingredients; }
 
     public void setIngredients(Set<Ingredient> ingredients) { this.ingredients = ingredients; }
+
+    public Difficulty getDifficulty() { return difficulty; }
+
+    public void setDifficulty(Difficulty difficulty) { this.difficulty = difficulty; }
 }
