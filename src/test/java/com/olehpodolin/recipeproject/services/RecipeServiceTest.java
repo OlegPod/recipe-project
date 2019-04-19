eacdaf13cba95a2518033cca910368623e000449
OlegPod <kepacuh@gmail.com>
2019-04-19 23:33:24 +0300

    Creating JUnit test and Using Mockito Mocks

---
+++ src/test/java/com/olehpodolin/recipeproject/services/RecipeServiceTest.java
@@ -0,0 +1,43 @@
+package com.olehpodolin.recipeproject.services;
+
+import com.olehpodolin.recipeproject.domain.Recipe;
+import com.olehpodolin.recipeproject.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceTest {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeService(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
